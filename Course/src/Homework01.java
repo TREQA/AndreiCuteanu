6330b197f13a539e4de70e3466e7197d41aaf967
acuteanu <andrei.cuteanu@tremend.com>
2017-12-11 17:21:54 +0200

    first commit

---
+++ Course/src/Homework01.java
@@ -0,0 +1,11 @@
+//enunt tema
+
+public class Homework01 {
+
+        public static void main(String[] args)
+        {
+            System.out.println("Goodbye!");
+            Homework01 t2 = new Homework01();
+        }
+    }
+
