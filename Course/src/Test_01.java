5592ed4ae5a151f9b2cf8a6cc648884f5ec0d45e
acuteanu <andrei.cuteanu@tremend.com>
2017-12-18 18:06:11 +0200

    Test_01

---
+++ Course/src/Test_01.java
@@ -0,0 +1,12 @@
+// Commit test01
+
+public class Test_01 {
+    public Test_01(){
+
+    }
+        public static void main(String[] args){
+
+        int a=63; int b=76;
+        System.out.println(a+b);
+        }
+}
