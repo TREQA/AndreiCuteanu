ed8f528be2250a09a241c97d02a2127ec1a3af21
avcuteanu <34440830+avcuteanu@users.noreply.github.com>
2017-12-18 18:12:37 +0200

    Update Homework04.java

--- Course/src/Homework04.java
+++ Course/src/Homework04.java
@@ -1,4 +1,21 @@
-// Exercitiul 2 din CJPR_03
+/* Exercitiul 2 din CJPR_03
+Se dă codul următor:
+int a=2,b=3;
+System.out.print("First operand: ");
+System.out.println(a);
+System.out.print("Second operand: ");
+System.out.println(b);
+System.out.print("Sum: ");
+System.out.println(a+b);
+
+Codul dă următoarea ieşire:
+First operand: 2
+Second operand: 3
+Sum: 5
+Trebuie să ne asigurăm că la ieşire codul afişează doar rezultatul
+(numărul 5) şi că toate liniile de cod rămân disponibile pentru o
+utilizare viitoare.
+*/
 
 public class Homework04 {
     public Homework04 (){
