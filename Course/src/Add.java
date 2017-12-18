8add2b2088e0da8efbb20aa50f1f5ca0e67ef481
avcuteanu <34440830+avcuteanu@users.noreply.github.com>
2017-12-18 18:16:26 +0200

    Update Add.java

--- Course/src/Add.java
+++ Course/src/Add.java
@@ -1,5 +1,18 @@
-//Exercitiul din CJPR01
+/* Exercitiul din CJPR01
+Haideți să încercăm să creăm încă un program Java. De data aceasta, programul va aduna două numere predefinite.
+Întâi vom crea programul. Acest program va fi asemănător cu programul din exemplul precedent:
 
+public class Add 
+{ 
+        public static void main(String[] args) { 
+/*metoda println()tipăreşte întregul 
+număr la ieşire şi trece în rând nou*/ 
+             System.out.println(2+3); 
+             } 
+}
+Salvăm acest program în sistemul de fişiere, sub numele de Add.java, şi apoi îl traducem cu compilatorul Java:
+
+*/
 
 public class Add
         {
