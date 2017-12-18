4e81989daf712f8a1930c95fc60f7b99dd51d483
acuteanu <andrei.cuteanu@tremend.com>
2017-12-18 17:56:17 +0200

    Homework01_E01

---
+++ Course/src/Homework04.java
@@ -0,0 +1,18 @@
+// Exercitiul 2 din CJPR_03
+
+public class Homework04 {
+    public Homework04 (){
+
+    }
+        public static void main(String[] args){
+
+        int a=2;int b=3;
+        System.out.print("First operand: ");
+        System.out.println(a);
+        System.out.print("Second operand: ");
+        System.out.println(b);
+        System.out.print("Sum: ");
+        System.out.println(a+b);
+
+        }
+}
