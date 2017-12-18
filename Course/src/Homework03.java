93b2812b68b9b1cc1f64c83720ecea711a48890f
acuteanu <andrei.cuteanu@tremend.com>
2017-12-18 17:45:31 +0200

    Homework03

---
+++ Course/src/Homework03.java
@@ -0,0 +1,23 @@
+public class Homework03 {
+    public Homework03() {
+
+    }
+        public static void main(String[] args) {
+        /*
+         * Primul sumator
+         */
+        int operand1 = 2;
+        /*
+         * Al doilea sumator
+         */
+        int operand2 = 3;
+        /*
+         * Calculul adunarii
+         */
+        int sum = operand1 + operand2;
+        /*
+         * afisarea rezultatului
+         */
+        System.out.println(sum);
+    }
+}
