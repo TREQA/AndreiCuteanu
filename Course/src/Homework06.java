8d5a38f24e41f886ade9a4887c9871870aca7262
acuteanu <andrei.cuteanu@tremend.com>
2017-12-19 12:53:08 +0200

    Homework05

---
+++ Course/src/Homework06.java
@@ -0,0 +1,15 @@
+/* Exercitiul 2 - CJPR04
+   Trebuie să declarăm două variabile. O variabilă de tip double cu
+valoarea 15,757 și o variabilă întreagă de tip int. Trebuie să convertiți
+variabila cu virgulă mobilă într-o valoare întreagă.
+*/
+public class Homework06 {
+    public Homework06() {
+    }
+        public static void main(String[] args) {
+        double x = 15.757;
+        int y = (int)x;
+
+        System.out.println(y);
+        }
+}
