2e5c07389cec6c45952d9982ef2aad8fe028ad3a
acuteanu <andrei.cuteanu@tremend.com>
2017-12-20 14:55:17 +0200

    Homework08

---
+++ Course/src/Homework08.java
@@ -0,0 +1,14 @@
+/* Exercitiul 2 - CJPR05
+    Editaţi numele constantei:
+public static final short pageWidth = 800;
+ */
+
+public class Homework08{
+    public static final short PAGE_WIDHT = 800;{
+    }
+        public static void main(String[] args){
+
+        System.out.printf("Page width is %d", PAGE_WIDHT);
+        }
+
+}
