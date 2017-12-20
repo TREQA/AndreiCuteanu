e4b9dd7e85a344b922670aa117a751aada4108b3
acuteanu <andrei.cuteanu@tremend.com>
2017-12-20 14:31:31 +0200

    Homework07

---
+++ Course/src/Homework07.java
@@ -0,0 +1,22 @@
+/* Exercitiul 1 - CJPR05
+
+Numele variabilelor trebuie schimbate astfel încât să fie corecte
+sintactic şi în acord cu convenţiile:
+
+String 1stUserName;
+int bdt; //date of birthday
+float o1; //first operand
+ */
+
+public class Homework07
+{
+    public static void main(String[] args)
+    {
+        String firstUserName = "avcuteanu";
+        int birthDate = 4/15/2000; //date of birthday
+        float operand1 = 4.1243534F; // first operand
+
+        System.out.println("Username is: " + firstUserName);
+        System.out.printf("Birthday is %d%nFirst operand id %.2f",birthDate,operand1);
+    }
+}
