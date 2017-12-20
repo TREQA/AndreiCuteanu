598ef9e4be98a7661a6d641efc6d4ac425b3bd6c
acuteanu <andrei.cuteanu@tremend.com>
2017-12-20 16:38:22 +0200

    Homework09

---
+++ Course/src/Homework09.java
@@ -0,0 +1,15 @@
+/* Exercitiul 1 - CJPR06
+Este necesar să creăm un program care adună două numere
+reprezentate prin două variabile. Primul număr este variabila x, cu
+valoarea 1, iar celălalt este variabila y, cu valoarea 2. Trebuie să
+atribuim variabilei z rezultatul adunării acestor două variabile.
+*/
+public class Homework09 {
+    public static void main(String[] args) {
+        int x = 1;
+        int y = 2;
+        int z = x + y;
+
+        System.out.println(z);
+    }
+}
