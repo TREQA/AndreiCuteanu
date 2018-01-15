d3ece0f42fd5e53e823d75b02dda4680754573c0
acuteanu <andrei.cuteanu@tremend.com>
2018-01-15 16:34:08 +0200

    Homework14

---
+++ Course/src/Homework14.java
@@ -0,0 +1,41 @@
+/* Curs: Core Java Programming
+   Modul: Controlul fluxului, buclele și ramificarea
+   Unitate: Controlul fluxului – If
+   Exercitiul 2:
+Trebuie modificat codul următor pentru a fi scris corect:
+if(x=y)
+{
+//logic goes here
+}
+else if(x=0);
+{
+//logic goes here
+}
+
+else(x)
+{
+//logic goes here
+}
+*/
+public class Homework14 {
+    public static void main(String[] Andrei) {
+        int x=3;
+        int y=1;
+
+        if (x==y)
+        {
+            //logic goes here
+            System.out.print("x=y");
+        }
+        else if(x==0)
+        {
+            //logic goes here
+            System.out.print("x=0");
+        }
+        else
+            {
+            //logic goes here
+            System.out.print("x != y");
+        }
+    }
+}
