9417becd2b4fd07c5a0ebbda37186cdbe101f400
acuteanu <andrei.cuteanu@tremend.com>
2018-01-15 16:21:36 +0200

    Homework13

---
+++ Course/src/Homework13.java
@@ -0,0 +1,14 @@
+/*Este necesară următoarea construcție logică: dacă x primește o
+valoare mai mare decât zero, se intră în blocul de cod și x este egal cu
+3.
+
+ */
+public class Homework13 {
+    public static void main (String[] Andrei){
+        int x = 15;
+
+        if (x>0) {
+            System.out.print("x=3");
+        }
+    }
+}
