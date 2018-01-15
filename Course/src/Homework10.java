936f661f4285125f3cd71ec4d69aa41b6caec8da
acuteanu <andrei.cuteanu@tremend.com>
2018-01-15 15:31:51 +0200

    Homework10

--- Course/src/Homework10.java
+++ Course/src/Homework10.java
@@ -1,7 +1,9 @@
 /* Exercitiul 2 - CJPR06
 Trebuie să creăm un program care calculează aria unui cerc.
  */
-
+/*
+dfs
+ */
 public class Homework10 {
     static final double PI=3.14;
     public static void main(String[] args) {
