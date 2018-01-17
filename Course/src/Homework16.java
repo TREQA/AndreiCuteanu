6eb3eca31f90e4fe4794820f6b6dd250b3ad26d5
acuteanu <andrei.cuteanu@tremend.com>
2018-01-17 12:23:36 +0200

    Homework16

---
+++ Course/src/Homework16.java
@@ -0,0 +1,40 @@
+/* Curs: Core Java Programming
+   Modul: Controlul fluxului, buclele și ramificarea
+   Unitate: Controlul fluxului - Switch
+
+   Exercitiul 2:
+  Avem următoarele date de intrare pentru un automobil:
+String carMake = "Ford";
+int doors = 4;
+  Trebuie să se facă o structură condițională care va verifica dacă
+producătorul automobilului este Ford și, dacă este, în funcție de
+numărul de uși, să afișeze mesajul adecvat.
+
+ */
+public class Homework16 {
+    public static void main(String[] Anrei){
+
+        String carMake = "Ford";
+        int doors = 4;
+
+        switch(carMake)
+        {
+            case "Ford":
+                switch(doors)
+                {
+                    case 3:
+                        System.out.println("You drive a car with three doors.");
+                        break;
+                    case 4:
+                        System.out.println("You drive a car with four doors.");
+                        break;
+                    case 5:
+                        System.out.println("You drive a car with five doors.");
+                        break;
+                }
+                break;
+                default:
+                System.out.println("You do NOT drive a Ford car.");
+        }
+    }
+}
