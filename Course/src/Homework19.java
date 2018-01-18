d14d055dc225f92b85cc6d770135d8dd35b4f4fb
avcuteanu <34440830+avcuteanu@users.noreply.github.com>
2018-01-18 10:43:27 +0200

    Update Homework19.java

--- Course/src/Homework19.java
+++ Course/src/Homework19.java
@@ -9,10 +9,10 @@ Trebuie să listăm şirul dat folosind bucla while.
 public class Homework19 {
     public static void main(String[] Andrei){
         int[] arr = {1,2,3,4,5,6,7,8,9,10};
-        int i = 0;
-        while(i<arr.length) {
-            System.out.println(arr[i]);
-            i++;
+        int x = 0;
+        while(x<arr.length) {
+            System.out.println(arr[x]);
+            x++;
         }
     }
 }
