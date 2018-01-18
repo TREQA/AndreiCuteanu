1818e6414a9d8691cfcd39a02dbd518e457cebd2
acuteanu <andrei.cuteanu@tremend.com>
2018-01-18 14:56:08 +0200

    Programarea orientată pe obiecte în Java - Elementele Clasei - Exercitiul 1

---
+++ Course/src/Homework22.java
@@ -0,0 +1,53 @@
+/* Curs: Core Java Programming
+   Modul: Programarea orientată pe obiecte în Java
+   Unitate: Elementele clasei
+   Exercitiul 1:
+
+Trebuie creată clasa Calculator (de buzunar) care are trei proprietăţi:
+operand1, operand2 şi operator. Clasa posedă metoda calculate() care,
+pe baza operatorului, execută operaţia de calcul adecvată asupra
+operanzilor şi returnează un rezultat. Sunt suficiente cele 4 operaţii de
+bază (+, -, *, / - adunarea, scăderea, înmulţirea şi împărţirea).
+Toate valorile sunt de tip double.
+*/
+
+public class Homework22 {
+    public static void main(String[] Andrei) {
+
+        Homework22 c = new Homework22();
+        c. operand1 = 2;
+        c. operand2 = 5;
+        c. operator = '+';
+        System.out.println(c.calculate());
+    }
+
+    double operand1;
+    double operand2;
+    char operator;
+
+    public double calculate() {
+        double result=0;
+        switch (this.operator) {
+            case '+':
+                result = this.operand1 + this.operand2;
+                break;
+            case '-':
+                result = this.operand1 - this.operand2;
+                break;
+            case '*':
+                result = this.operand1 * this.operand2;
+                break;
+            case '/':
+                if (this.operand2 == 0) {
+                    System.out.println("Împărțirea nu este posibilă!");
+                    result = 0;
+                } else
+                    result = this.operand1 / this.operand2;
+                break;
+            default:
+                result = 0;
+                break;
+        }
+        return result;
+    }
+}
