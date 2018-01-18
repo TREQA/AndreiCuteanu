6089309f0de6be3f39928213b33bc8ec7474d77f
acuteanu <andrei.cuteanu@tremend.com>
2018-01-18 10:00:17 +0200

    Homework18

--- Course/src/Homework18.java
+++ Course/src/Homework18.java
@@ -16,12 +16,9 @@ public class Homework18
         String[][] users = { { "petar" , "petrovic" , "1111" },{"jovan","jovanovic","2222"} , {"zoran","ivanovic","3333"}};
         String[] names = new String[users.length];
 
-        for(int i=0; i<users.length; i++)
-            names[i] = users[i][0];
-        System.out.println(java.util.Arrays.toString(names));
-
-        for(int i=0; i<users.length; i++)
-            names[i] = users[i][1];
+        for(int i=0; i<users.length; i++) {
+                names[i] = users[i][0];
+        }
         System.out.println(java.util.Arrays.toString(names));
     }
 }
