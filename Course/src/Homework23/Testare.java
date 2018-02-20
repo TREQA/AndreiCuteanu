5acd2912277d27c153bbdf6ca21294c319ade803
avcuteanu <34440830+avcuteanu@users.noreply.github.com>
2018-02-20 16:21:18 +0200

    OOP in java (13.2)

--- Course/src/Homework23/Testare.java
+++ Course/src/Homework23/Testare.java
@@ -1,3 +1,15 @@
+/* Curs: Core Java Programming
+   Modul: Programarea orientată pe obiecte în Java
+   Unitate: Elementele clasei
+   Exercitiul 2:
+Trebuie create 3 clase. Prima este clasa Circle şi ea conţine
+proprietatea r (raza). A doua clasă este Rectangle şi conţine
+proprietăţile a şi b. A treia clasă se numeşte Calc şi conţine metoda
+statică area care, ca parametru, acceptă o instanţă a clasei (Circle sau
+Rectangle), iar ca rezultat prezintă aria formei introduse. În funcţie
+dacă în metodă a intrat instanţa clasei Circle sau Rectangle, va fi
+executată formula de calculare a ariei pentru Circle sau Rectangle.
+*/
 package Homework23;
 
 public class Testare {
