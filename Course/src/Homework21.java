2b83aee87d323c098dad33506c07bba98a4f46b8
acuteanu <andrei.cuteanu@tremend.com>
2018-02-20 18:13:08 +0200

    OOP in Java (12.1 + 12.2)

--- Course/src/Homework21.java
+++ Course/src/Homework21.java
@@ -2,13 +2,13 @@
    Modul: Programarea orientată pe obiecte în Java
    Unitate: Clase şi obiecte
    Exercitiul 1 + Exercitiul 2
-E1: Trebuie creată clasa Animal care va fi definită ca una abstractă, iar
+Ex.1: Trebuie creată clasa Animal care va fi definită ca una abstractă, iar
 apoi trebuie create două clase, Horse şi Dog, care vor moşteni prima
 clasă Animal.
-E2: Trebuie executată instanţierea claselor din exerciţiul precedent.
+Ex.2: Trebuie executată instanţierea claselor din exerciţiul precedent.
  */
 public class Homework21 {
-    public static void main(String[] Andrei) {
+    public static void main(String[] args) {
         abstract class Animal {
             class Horse extends Animal {
                 Horse horse = new Horse();
