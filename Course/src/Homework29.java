79914b7a0542e486841252e18592a9ef119918a9
avcuteanu <34440830+avcuteanu@users.noreply.github.com>
2018-02-22 17:09:56 +0200

    Tipurile de date obiectuale (17.2)

--- Course/src/Homework29.java
+++ Course/src/Homework29.java
@@ -1,3 +1,17 @@
+/* Curs: Core Java Programming
+   Modul: Tipurile de date obiectuale
+   Unitate: Stringurile
+   Exercitiul 2:
+În aplicație intră o listă de fișiere sub forma unui string. Trebuie recunoscute doar fișierele de tip jpg, png sau gif (prin utilizarea
+extensiilor acestora) și afișat un mesaj despre tipul lor la ieșire:
+
+Fișierul: picture1.jpg este de tip jpg
+Fișierul: picture2.png este de tip png
+Fișierul: picture3.gif este de tip gif
+
+Parametrul de intrare:
+String[] files = { "picture1.jpg", "text1.txt", "picture2.png", "text2.doc", "picture3.gif" };
+*/
 public class Homework29 {
     public static void main(String[] args){
         String[] files = { "picture1.jpg", "text1.txt", "picture2.png", "text2.doc", "picture3.gif" };
