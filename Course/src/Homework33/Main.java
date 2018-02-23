d2c8c8d1b6cd0626066b65a815bc3010f96ff02a
acuteanu <andrei.cuteanu@tremend.com>
2018-02-23 18:10:55 +0200

    Tipurile de date obiectuale (19.1)

--- Course/src/Homework33/Main.java
+++ Course/src/Homework33/Main.java
@@ -1,3 +1,39 @@
+/*  Curs: Core Java Programming
+    Modul: Tipurile de date obiectuale
+    Unitate: Colecțiile
+    Exercitiul 1:
+Avem o clasă User:
+public class User {
+public int Id;
+public String name;
+public User(int id, String name)
+{
+this.Id = id;
+this.ime = name;
+}
+}
+
+De asemenea, în metoda Main a aplicației, avem următorul cod:
+
+...
+ArrayList usersToAdd = new ArrayList();
+usersToAdd.add(new User(4,"Goran"));
+usersToAdd.add(new User(2,"Ilija"));
+usersToAdd.add(new User(6,"Nikola"));
+
+ArrayList users = new ArrayList ();
+users.add(new User(1, "Petar"));
+users.add(new User(2, "Jovan"));
+users.add(new User(3, "Zoran"));
+
+...
+Adăugați toți utilizatorii din lista usersToAdd în lista Users. Dacă există
+deja un utilizator cu același ID, înlocuiți vechiul utilizator cu cel nou.
+
+Afisati lista Users.
+
+ */
+
 package Homework33;
 
 import java.util.ArrayList;
