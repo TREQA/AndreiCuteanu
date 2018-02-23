f1dfe4a0a244af25e34ea239d3dee08ee3123abf
acuteanu <andrei.cuteanu@tremend.com>
2018-02-23 18:13:31 +0200

    Tipurile de date obiectuale (18.1)

--- Course/src/Homework32/Main.java
+++ Course/src/Homework32/Main.java
@@ -1,3 +1,18 @@
+/*  Curs: Core Java Programming
+    Modul: Tipurile de date obiectuale
+    Unitate: Enumerările
+    Exercitiul 1:
+În cadrul aplicației noastre web am obținut următorul string care transmite parametri în următoru format:
+
+serverId=25&serverName=my name&serverStatus=Running
+
+Creați o clasă Server cu următoarele câmpuri: Id, Name and Status.
+Creați o enumerare cu statusurile serverului (Stopped, Running, Unknown) și o metodă care izolează
+parametrii (din probleme) și creează o instanță a clasei Server pe baza lor.
+Trebuie suprascrisă metoda toString a clasei Server și afișat ID-ul, numele și statusul serverului în ea.
+Realizați soluții de test prin intermediul metodei Main a clasei Main.
+
+ */
 package Homework32;
 
 public class Main {
