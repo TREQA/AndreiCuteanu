5daf357beea9f342174097012f707dfb66552445
acuteanu <andrei.cuteanu@tremend.com>
2018-02-23 16:32:52 +0200

    Tipurile de date obiectuale (18.1)

---
+++ Course/src/Homework32/Main.java
@@ -0,0 +1,8 @@
+package Homework32;
+
+public class Main {
+    public static void main(String[] args){
+        Server s = new Server("serverId=25&serverName=my name&serverStatus=Stopped");
+        System.out.println(s);
+    }
+}
