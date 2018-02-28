f1b8a83662e0ab2f5c9de4a41489ad771aeff278
acuteanu <andrei.cuteanu@tremend.com>
2018-02-28 12:09:52 +0200

    Gestionarea sistemului de fisiere (22.1)

--- Course/src/Homework38/Main.java
+++ Course/src/Homework38/Main.java
@@ -1,9 +1,32 @@
+/*  Curs: Core Java Programming
+    Modul: Gestionarea sistemului de fișiere
+    Unitate: Fluxurile de date
+    Exercitiul 2:
+Avem fișierul users.txt cu următorul conținut:
+
+id:01|firstName:Petar|lastName:Petrovic|jmbg:1234567890123
+id:02|firstName:Jovan|lastName:Jovanovic|jmbg:1234567890124
+id:03|firstName:Nikola|lastName:Nikolic|jmbg:1234567890125
+
+Creați o aplicație care va citi fișierul și va afișa conținutul în următorul format:
+
+User Id: 01
+First name: Petar
+Last name: Petrovic
+Jmbg: 1234567890123
+------------------------------------
+User Id: 02
+First name: Jovan
+Last name: Jovanovic
+Jmbg: 1234567890124
+.....
+ */
 package Homework38;
 
 import java.io.*;
 public class Main {
     public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\acuteanu\\Desktop\\users.txt"));
+        BufferedReader br = new BufferedReader(new FileReader("C:\\Users\\acuteanu\\IdeaProjects\\AndreiCuteanu\\Course\\src\\Homework38\\users.txt"));
         String line;
         while((line=br.readLine())!=null)
         {
